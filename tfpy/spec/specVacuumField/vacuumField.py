9fa25199acfc45e0d204a59b5301628b8dd5f323
Ke Liu <lk2020@mail.ustc.edu.cn>
2025-03-12 21:21:25 +0800

    SPECVacuumField

--- tfpy/spec/specVacuumField/vacuumField.py
+++ tfpy/spec/specVacuumField/vacuumField.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# laplaceField.py
+# vacuumField.py
 
 
+from typing import List
+from tfpy.geometry import Surface_BoozerAngle
 from ..specOut import SPECOut
 
 
-class LaplaceFiled: 
+class SPECVacuumField: 
     
     def __init__(self, file: str) -> None:
         self.data = SPECOut(file)
         assert self.data.input.physics.nvol == 1 
         self.nfp = self.data.input.physics.nvol
+    
+    # TODO
+    def getBoozerSurfaces(self, tflux: List[float]) -> List[Surface_BoozerAngle]:
+        pass
         
 
 
