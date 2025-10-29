311f607a230b67833f48f8dd5dd5a15f52175293
Ke Liu <lk2020@mail.ustc.edu.cn>
2025-10-29 10:51:18 +0800

    rename

--- tfpy/geometry/potentialCylinderAngle.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# potentialCylinderAngle.py
-
-
-import numpy as np
-from .surfaceCylindricalAngle import Surface_cylindricalAngle
-from ..toroidalField import ToroidalField
